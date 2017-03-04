fc11e997bbe06dd778da8f40922d9e373e1fe1dd
yswu1022eng <yswu1022eng@gmail.com>
2017-03-04 23:06:56 +0800

    初始化專案

---
+++ CS280-Homework-1.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS280_Homework_1._1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
